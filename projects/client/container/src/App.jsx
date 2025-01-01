94fdc20a5f6cae01daca0d95ff02d7863674edc9
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2025-01-01 14:56:56 +0700

    fix container

--- projects/client/container/src/App.jsx
+++ projects/client/container/src/App.jsx
@@ -16,7 +16,6 @@ const App = () => {
         <Route path="/payment/callback" element={<PaymentApp />} />
         <Route path="/user/*" element={<UserProfileApp />} />
       </Routes>
-      {/* <Footer /> */}
     </div>
   );
 };
