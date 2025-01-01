16bc906ecde7f88c4843b552580d165b9a1b5fde
TuytMai <tranthituyetmai7430@gmail.com>
2025-01-01 18:23:10 +0700

    fix hotel card

--- projects/client/booking-mf/src/components/HotelCard.jsx
+++ projects/client/booking-mf/src/components/HotelCard.jsx
@@ -7,34 +7,36 @@ const HotelCard = ({ hotel }) => {
   return (
     <div className="border">
       <img src={hotel.thumbnail} className="w-full h-[260px] object-cover" />
-      <div className="p-4 space-y-3">
+      <div className="p-4 flex flex-col gap-3">
         <p className="uppercase text-sm">{hotel.type}</p>
-        <div className="flex">
-          <h3 className="text-xl font-semibold line-clamp-2 flex-1">
-            {hotel.name}
-          </h3>
-          {/* <button>
+        <div className="flex-1 space-y-3">
+          <div className="flex">
+            <h3 className="text-xl font-semibold line-clamp-2 flex-1">
+              {hotel.name}
+            </h3>
+            {/* <button>
             <Heart size={24} />
           </button> */}
-        </div>
-        <div className="flex items-center gap-2 text-sm text-slate-400">
-          <Star size={16} stroke="#FDBE02" fill="#FDBE02" />
-          <p>{hotel.rating > 0 ? hotel.rating.toFixed(2) : 0}/5</p>
-          <p>{hotel.rating_count} đánh giá</p>
-        </div>
-        <div className="flex items-center gap-2 text-sm text-slate-400">
-          <Wifi size={16} />
-          <p className="flex-1">
-            {hotel.amenities.map((amenity) => amenity.name).join(", ")}
-          </p>
-        </div>
-        <div className="flex items-center gap-2 text-sm text-slate-400">
-          <MapPin size={16} className="mb-1" />
-          <p className="flex-1">{hotel.location.province.name}</p>
-        </div>
-        <div className="flex items-center gap-2 text-sm text-slate-400">
-          <DoorOpen size={16} className="mb-1" />
-          <p className="flex-1">{hotel.rooms.length} phòng</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-slate-400">
+            <Star size={16} stroke="#FDBE02" fill="#FDBE02" />
+            <p>{hotel.rating > 0 ? hotel.rating.toFixed(2) : 0}/5</p>
+            <p>{hotel.rating_count} đánh giá</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-slate-400">
+            <Wifi size={16} />
+            <p className="flex-1">
+              {hotel.amenities.map((amenity) => amenity.name).join(", ")}
+            </p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-slate-400">
+            <MapPin size={16} className="mb-1" />
+            <p className="flex-1">{hotel.location.province.name}</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-slate-400">
+            <DoorOpen size={16} className="mb-1" />
+            <p className="flex-1">{hotel.rooms.length} phòng</p>
+          </div>
         </div>
         <div className="flex gap-2 items-center">
           <p className="text-xl font-semibold flex-1">
